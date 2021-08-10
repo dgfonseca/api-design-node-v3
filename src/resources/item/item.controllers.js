d66fd9a92eac0317b1237b6b5ff3971536d522a7
David Fonseca <dg.fonseca@uniandes.edu.co>
2021-08-09 20:41:37 -0500

    Lesson 2 finished'

--- src/resources/item/item.controllers.js
+++ src/resources/item/item.controllers.js
@@ -1,1 +1,19 @@
-export default {}
+export const getItems = (req, res) => {
+  res.send({ items: [1, 2, 3] })
+}
+
+export const postItem = (req, res) => {
+  res.send({ body: req.body })
+}
+
+export const getItemById = (req, res) => {
+  res.send({ body: req.params.id })
+}
+
+export const deleteItemById = (req, res) => {
+  res.send({ body: req.params.id })
+}
+
+export const putItemById = (req, res) => {
+  res.send({ body: req.params.id })
+}
--- src/resources/item/item.router.js
+++ src/resources/item/item.router.js
@@ -1,5 +1,22 @@
 import { Router } from 'express'
 
+import {
+  deleteItemById,
+  getItemById,
+  getItems,
+  postItem,
+  putItemById
+} from './item.controllers'
 const router = Router()
+
+router
+  .route('/')
+  .get(getItems)
+  .post(postItem)
+router
+  .route('/:id')
+  .get(getItemById)
+  .put(putItemById)
+  .delete(deleteItemById)
 
 export default router
