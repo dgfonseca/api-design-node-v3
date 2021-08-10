ec4a1c8fa0537b011f7fda1144cae7b4750d87b2
David Fonseca <dg.fonseca@uniandes.edu.co>
2021-08-09 20:02:15 -0500

    Examples added

--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ import morgan from 'morgan'
 import cors from 'cors'
 
 export const app = express()
+const router = express.Router()
 
 app.disable('x-powered-by')
 
@@ -11,5 +12,45 @@ app.use(cors())
 app.use(json())
 app.use(urlencoded({ extended: true }))
 app.use(morgan('dev'))
+app.use('/api', router)
 
-export const start = () => {}
+const routes = [
+  'get /cat',
+  'get /cat/:id',
+  'post /cat',
+  'put /cat/:id',
+  'delete /cat/:id'
+]
+
+// Simplificar para cada ruta no tener que crear el metodo por aparte.
+router
+  .route('/cat')
+  .get()
+  .put()
+  .delete()
+
+// Custom middleware for logging and using next for calling next middleware in the stack
+const log = (req, res, next) => {
+  console.log('logging')
+  next()
+}
+
+// Subrouting with the route /route
+router.get('/route', (req, res) => {
+  res.send({ me: 'ok' })
+})
+
+app.get('/', log, (req, res) => {
+  res.send({ message: 'hello' })
+})
+
+app.post('/', (req, res) => {
+  console.log(req.body)
+  res.send({ message: 'Ok' })
+})
+
+export const start = () => {
+  app.listen(3000, () => {
+    console.log('server started in port 3000')
+  })
+}
